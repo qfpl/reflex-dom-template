52bb3970d0cb7c569565fc738b5c637063e6cb52
Dave Laing <dave.laing.80@gmail.com>
2018-10-10 10:49:55 +1000

    Bumps reflex-platform and some bounds

--- default.nix
+++ default.nix
@@ -5,6 +5,12 @@ let
 
   pkgs = reflex-platform.nixpkgs.pkgs;
   ghc = reflex-platform.${compiler};
-  drv = ghc.callPackage ./reflex-dom-template.nix {};
+  haskellPackages = ghc.override {
+    overrides = self: super: {
+      # working around this: https://github.com/nikita-volkov/html-entities/issues/8
+      html-entities = pkgs.haskell.lib.dontCheck (self.callHackage "html-entities" "1.1.4.1" {});
+    };
+  };
+  drv = haskellPackages.callPackage ./reflex-dom-template.nix {};
 in
   drv
--- example/default.nix
+++ example/default.nix
@@ -3,10 +3,6 @@
 }:
 with import ./.obelisk/impl { inherit system iosSdkVersion; };
 project ./. ({ ... }: {
-  android.applicationId = "systems.obsidian.obelisk.examples.minimal";
-  android.displayName = "Obelisk Minimal Example";
-  ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
-  ios.bundleName = "Obelisk Minimal Example";
   overrides = self: super: {
     reflex-dom-template = super.callCabal2nix "reflex-dom-template" ../. {};
   };
